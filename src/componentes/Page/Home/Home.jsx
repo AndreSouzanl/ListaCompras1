43c1eada5b322f2dc2b572ae23bed2b2cbeffbfd
Vinicius <vinicius.barriquelo@octalink.com.br>
2023-11-13 09:05:09 -0300

    Integração.

--- src/componentes/Page/Home/Home.jsx
+++ src/componentes/Page/Home/Home.jsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../Hedear/Header";
 import Lista from "../Item/Lista";
 import "./style.css";
-
-const listaItens = [
-  { id: "1", name: "camisa", data: "15/11/2023" },
-  { id: "2", name: "calça", data: "11/11/2023" },
-];
+import axios from 'axios';
 
 function Home() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [name, setName] = useState(null)
+  const [data, setData] = useState(null)
+
+  const url = 'https://lista-compras-api-production.up.railway.app/lista'
+
+  useEffect(() => {
+    getList()
+  }, [])
+
+  function getList() {
+    setLoading(true)
+
+    axios.get(url)
+    .then(function (response) {
+      console.log(response)
+      setList(response.data.map(item => {
+        const data = new Date(item.data).toLocaleString('pt-BR');
+  
+        return {
+          ...item,
+          data
+        }
+      }));
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      setLoading(false)
+    });
+  }
+  
+  function createList() {
+    axios.post(url, {nome: name, data, data})
+    .then(response => {
+      fecharModal()
+      getList()
+    })
+  }
 
   function abrirModal() {
     setOpen(true);
   }
+
   function fecharModal() {
     setOpen(false);
   }
@@ -22,11 +61,17 @@ function Home() {
     <>
       <Header titulo="Listas" acaotext="Abrir Modal" acao={abrirModal} />
 
-      <ul>
-        {listaItens.map((item) => {
-          return <Lista item={item} />;
-        })}
-      </ul>
+      {list.length === 0 && <h1>Não tem listas cadastradas</h1>}
+
+      {
+        loading ?
+          <h1>Loading</h1>
+          : <ul>
+            {list.map((item) => {
+              return <Lista key={item.id} item={item} />;
+            })}
+          </ul>
+      }
 
       {open && (
         <div className="modal">
@@ -37,15 +82,15 @@ function Home() {
             <form action="#">
               <div className="form-item">
                   <label>Nome</label>
-                  <input type="text" />
+                  <input type="text" onInput={(e) => setName(e.target.value)}/>
               </div>
 
               <div className="form-item">
                   <label>data</label>
-                  <input type="date" />
+                  <input type="date"  onInput={(e) => setData(e.target.value)}/>
               </div>
 
-              <button>Salvar</button>
+              <button onClick={() => createList()}>Salvar</button>
             </form>
           </div>
         </div>
--- src/componentes/Page/Item/Lista.jsx
+++ src/componentes/Page/Item/Lista.jsx
@@ -3,7 +3,7 @@ import './style.css'
 function Lista(props) {
   return (
     <li>
-      <p>{props.item.name}</p>
+      <p>{props.item.nome}</p>
       <p>{props.item.data}</p>
     </li>
   );
